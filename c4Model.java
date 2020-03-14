413102c778616373c83c8497c4fb4ac09c267850
dalezw <49211029+dalezw@users.noreply.github.com>
2020-03-14 13:45:28 -0400

    final upload, still needs connected to main(controller)

--- c4Model.java
+++ c4Model.java
@@ -2,20 +2,19 @@
  * 
  * @author dalezw
  * Date: 01 march 2020
+ * Last Modified: 14 march 2020
  */
 
 package mvc;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import javax.swing.JPanel;
 public class c4Model {
 
 	private int[][] board= new int[6][7];// game board
 	
 	int player=1;
 	
-	public void reset() {	
+	//sets board up with all values of 0 
+	public void setBoard() {	
 		for(int[] i: board) {
 			for (int j=0; j<i.length; j++) {
 				i[j]=0;
@@ -23,15 +22,7 @@ public class c4Model {
 		}
 	}
 	
-	public void changeTurn() {
-		 if(player==2) {
-			 player=1;
-		 }
-		 else {
-			 player=2;
-		 }
-	}
-	
+	// checks for win
 	public boolean winCondition() {
 		
 		boolean horiz = checkHorizontal();
@@ -52,6 +43,7 @@ public class c4Model {
 		}
 	}
 	
+	//checks for horizontal win
 	public boolean checkHorizontal() {
 		for(int r=0; r<4;r++) {
 			for(int c=0;c<6;c++) {
@@ -65,6 +57,7 @@ public class c4Model {
 		return false;
 	}
 	
+	//checks for vertical win
 	public boolean checkVertical() {
 		for(int r=0; r<3;r++) {
 			for(int c=0;c<7;c++) {
@@ -78,6 +71,7 @@ public class c4Model {
 		return false;
 	}
 	
+	//checks for diagonal win
 	public boolean checkDiagonal() {
 		int r=0, c=0;
 		//positive slope
@@ -102,10 +96,57 @@ public class c4Model {
 		return false;
 	}
 	
-	public void insertPiece(String column){
+	//finds open slot for insertion of piece and sets that slot to the current player
+	public void findOpenSlot(int column) {
+		boolean done=false;
 		
-		
+		while (!done) {
+			for(int r=6; r>0;r--) {
+				if(board[r][column]==0) {
+					board[r][column]=player;
+					done=true;
+				}
+			}
+		}
 	}
+	
+	//takes input of number from player and uses findOpenSlot to input piece in lowest open slot
+	public void insertPiece(String column) {
+		switch(column) {
+		case"1":
+			int column1=0;
+			this.findOpenSlot(column1);
+			break;
+		case"2":
+			int column2=1;
+			this.findOpenSlot(column2);
+			break;
+		case"3":
+			int column3=2;
+			this.findOpenSlot(column3);
+			break;
+		case"4":
+			int column4=3;
+			this.findOpenSlot(column4);
+			break;
+		case"5":
+			int column5=4;
+			this.findOpenSlot(column5);
+			break;
+		case"6":
+			int column6=5;
+			this.findOpenSlot(column6);
+			break;
+		case"7":
+			int column7=6;
+			this.findOpenSlot(column7);
+			break;
+		}
+	}
+	
+	
+	
+	
 	
 
 }
