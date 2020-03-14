7255b759b63d4ea3981bcf9b4ec74e1c97284543
DYokajty <31545519+DYokajty@users.noreply.github.com>
2020-03-14 15:47:59 -0400

    Connected to Main

--- View.java
+++ View.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class View {
 
 	Scanner userInput = new Scanner(System.in);
+	int currentPlayer;
 	
 	public void displayBoard(int[][] board) {
 		for(int c = 0; c < 7; c++) {
@@ -13,6 +14,7 @@ public class View {
 	}
 	
 	public int getPlayerMove(int player) {
+		currentPlayer = player;
 		if (player == 1) {
 			System.out.println("Player 1, which column would you like to place your piece in?\nPick a number between 1 and 7");
 		} else {
@@ -20,5 +22,23 @@ public class View {
 		}
 		
 		return userInput.nextInt();
+	}
+	
+	public void invalidMove() {
+		System.out.println("Invalid move, please select a column with open spaces");
+		getPlayerMove(currentPlayer);
+	}
+	
+	public void draw() {
+		System.out.println("The game results in a draw.");
+	}
+	
+	public void winner(int player) {
+		currentPlayer = player;
+		if (player == 1) {
+			System.out.println("Player 1 wins!");
+		} else {
+			System.out.println("Player 2 wins!");
+		}
 	}
 }
