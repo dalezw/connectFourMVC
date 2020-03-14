d248ea819c1111bd46aa4d5af053cab0632fd75f
DYokajty <31545519+DYokajty@users.noreply.github.com>
2020-03-14 15:47:39 -0400

    Connected everything

--- Main.java
+++ Main.java
@@ -1,45 +1,38 @@
-package mvc;
-
 
 public class Main {
-
-c4Model game;
 	
-	public Main() {
-		game=new c4Model();
-	}
-	
-	private int[][] board = new int[6][7];
+	c4Model game;
+	private int[][] board = new int[7][6];
 	private int turnCounter = 1;
-	private boolean winCondition = game.winCondition();
-	private int row, col;
+	private boolean winCondition  = game.winCondition();
+	private int row, col, userChoice;
 	private int currentPlayer;
+	View view;
 	
 	
-
-	while (game.winCondition == false) {
+	while (winCondition == false) {
 		if (turnCounter%2 == 0) {
 			currentPlayer = 2;
 		} else {
 			currentPlayer = 1;
 		}
-		//get input from GUI on the column selected
-		//set to column
 		
-		if (board[column][7] != 0) {
-			
+		userChoice = view.getPlayerMove(currentPlayer);
+		
+		if (board[userChoice][6] != 0) {
+			view.invalidMove();
 		}
 		
-		for(int i = 0; i < 7; i++) {
-			if (board[column][i] == 0) {
-				board[column][i] = currentPlayer;
+		for(int i = 0; i < 6; i++) {
+			if (board[userChoice][i] == 0) {
+				board[userChoice][i] = currentPlayer;
 				System.out.println("Piece added");
 				break;
 			}
 		}
 		
 		
-		if (winCondition == true) {  
+		if (game.winCondition() == true) {
 			winCondition = true;
 		} else if (turnCounter > 42) {  //No more moves can be made and the game results in a Draw
 			winCondition = true;
@@ -47,13 +40,14 @@ c4Model game;
 		turnCounter += 1;
 	}
 	
-	if (turnCounter > 42 || game.winCondition()==true ) {
-		System.out.println("Draw");
+	if (turnCounter > 42) {
+		view.draw();
 	} else if (turnCounter % 2 == 0) { //If the turnCounter is even then Player 2 won
-		System.out.println("Player 2 Wins");
+		view.winner(2);
 	} else {
-		System.out.println("Player 1 Wins");
+		view.winner(1);
 	}
-}	
+	
+}
 }
 
