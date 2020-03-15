eed68fd4ce30a43f9e7ab38aced5b5f57ed17af6
DYokajty <31545519+DYokajty@users.noreply.github.com>
2020-03-14 22:09:13 -0400

    Fixed the orientation graphically

--- View.java
+++ View.java
@@ -5,20 +5,25 @@ public class View {
 	Scanner userInput = new Scanner(System.in);
 	int currentPlayer;
 	
+	public View() {
+		//constructor
+	}
+	
 	public void displayBoard(int[][] board) {
-		for(int c = 0; c < 7; c++) {
-			for (int r = 0; r < 6; r++) {
-				System.out.print(board[c][r]+"|  ");
+		for(int r = 5; r >= 0; r--) {
+			for (int c = 0; c < 7; c++) {
+				System.out.print(board[c][r]+" | ");
 			}
+			System.out.println();
 		}
 	}
 	
 	public int getPlayerMove(int player) {
 		currentPlayer = player;
 		if (player == 1) {
-			System.out.println("Player 1, which column would you like to place your piece in?\nPick a number between 1 and 7");
+			System.out.println("Player 1, which column would you like to place your piece in? Pick a number between 1 and 7");
 		} else {
-			System.out.println("Player 2, which column would you like to place your piece in?\\nPick a number between 1 and 7");
+			System.out.println("Player 2, which column would you like to place your piece in? Pick a number between 1 and 7");
 		}
 		
 		return userInput.nextInt();
